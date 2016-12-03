bcd55d65d4e9c73e5e9e76cb0b97cf9e29ad55b6
javier muñoz <javier_munoz_navarrete@hotmail.com>
2016-12-03 02:31:57 -0300

    se agrega role

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-		enum role: [:client, :corredor_in, :corredor_out]
+		enum role: [:client, :corredor_in, :corredor_out, :admin]
 end
