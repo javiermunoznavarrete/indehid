74d1d714187f7449fc58644c05c2ca01ef777b45
javier muñoz <javier_munoz_navarrete@hotmail.com>
2016-12-15 20:01:19 -0300

    desde aqui me ordeno con los commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 		enum role: [:client, :corredor_in, :corredor_out, :admin]
+	validates :user,:address, :city, :phone, :comune, :country, presence: true
 end
