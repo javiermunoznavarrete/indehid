b8b62556247a118797dd58a0c28dd69db9ab28ca
javier muñoz <javier_munoz_navarrete@hotmail.com>
2017-01-07 13:33:58 -0300

    se agrega mailer de rails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,17 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  ActionMailer::Base.smtp_settings = {
+    :address => "mail.indehid.com ",
+    :port => 465,
+    :authentication => :plain,
+    :domain => 'indehid.com',
+    :user_name => ENV['email'],
+    :password => ENV['email_password'],
+  }
 end
